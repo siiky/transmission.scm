458308b98eabed703a71612b97b8d91c61d48e21
siiky <siiky@users.noreply.github.com>
2019-08-16 16:22:21 +0100

    Start a transmission-daemon for testing

---
+++ Makefile
@@ -0,0 +1,67 @@
+# Build Variables
+
+EGG_SRC := transmission.scm
+EXAMPLE_SRC := example.scm
+SRC := $(EGG_SRC) $(EXAMPLE_SRC)
+
+default: $(EGG_SRC)
+	chicken-install -n
+
+clean:
+	chicken-clean
+
+lint: $(SRC)
+	chicken-lint $(SRC)
+
+# Transmission Daemon Settings
+
+AUTH_NO_AUTH := --auth # --no-auth
+CONFIG_DIR := transmission_config
+DHT_NO_DHT := --no-dht # --dht
+DOWNLOAD_DIR := transmission_download
+ENCRYPTION := --encryption-required
+LOGGING := --log-debug
+LPD_NO_LPD := --no-lpd # --lpd
+PASSWORD := password
+PEER_PORT := 50000
+PORTMAP_NO_PORTMAP := --no-portmap # --portmap
+RPC_PORT := 4242
+USERNAME := username
+
+LOG_FILE := transmission.log
+
+TRANSMISSION_ARGS := \
+    $(AUTH_NO_AUTH) \
+    $(ENCRYPTION) \
+    $(LOGGING) \
+    $(PORTMAP_NO_PORTMAP) \
+    --config-dir $(CONFIG_DIR) \
+    --download-dir $(DOWNLOAD_DIR) \
+    --no-incomplete-dir \
+    --no-watch-dir \
+    --password $(PASSWORD) \
+    --peerport $(PEER_PORT) \
+    --port $(RPC_PORT) \
+    --username $(USERNAME) \
+    -f \
+
+CSI_ARGS := \
+    --username $(USERNAME) \
+    --password $(PASSWORD) \
+    --port $(RPC_PORT) \
+
+$(DOWNLOAD_DIR):
+	mkdir $(DOWNLOAD_DIR)
+
+csi: $(EXAMPLE_SRC)
+	[ -f start-transmission ] && csi -q -s example.scm --repl $(CSI_ARGS)
+
+example: $(EXAMPLE_SRC)
+	[ -f start-transmission ] && csi -q -s example.scm $(CSI_ARGS)
+
+start-transmission: $(DOWNLOAD_DIR)
+	touch start-transmission
+	transmission-daemon $(TRANSMISSION_ARGS) 2>&1 | tee $(LOG_FILE)
+	rm start-transmission
+
+.PHONY: clean csi default example lint
--- example.scm
+++ example.scm
@@ -9,9 +9,14 @@
 ;;; }
 ;;; ```
 
+(import chicken.pretty-print)
 (import chicken.process-context)
-(import transmission)
+(import chicken.repl)
+
+(import optimism)
 (import srfi-1)
+
+(import transmission)
 
 (define unique-tag
   (let ((n 0))
@@ -20,49 +25,54 @@
         (set! n (+ n 1))
         ret))))
 
-(define (maybe-list-ref l n)
-  (cond
-    ((null? l) #f)
-    ((zero? n) (car l))
-    (else (maybe-list-ref (cdr l) (- n 1)))))
+(define (nop . x) #f)
 
-(define (maybe-rest-args args)
-  (if (maybe-list-ref args 2)
-      (cddr args)
-      '()))
+(define (torrent-get-example)
+  (display "torrent-get-example: ")
+  (print-result (torrent-get '("id" "totalSize") #:tag (unique-tag))))
 
-(define (torrent-get-example args)
-  (let ((result (torrent-get '("id" "totalSize"))))
-    (print-result result (lambda (pn) (print pn  " [USERNAME PASSWORD]")))))
+(define (session-get-example)
+  (display "session-get-example: ")
+  (print-result (session-get #:tag (unique-tag))))
 
-(define (torrent-start-example args)
-  (print-result (torrent-start #:ids args #:tag (unique-tag))
-                (lambda (pn) (print pn " USERNAME PASSWORD [IDS ...]"))))
+(define (print-result result)
+  (when result ; rpc-call returns #f on wrong parameters
+    (let ((reply (vector->list result)))
+      (let ((arguments (cdr (assoc "arguments" reply)))
+            (result (cdr (assoc "result" reply))))
+        (if (string=? result "success")
+            (pp arguments)
+            (pp result))))))
 
-(define (torrent-stop-example args)
-  (print-result (torrent-stop #:ids args #:tag (unique-tag))
-                (lambda (pn) (print pn " USERNAME PASSWORD [IDS ...]"))))
+(define *OPTS*
+  `(((--port) . ,string->number)
+    ((--username) . username)
+    ((--password) . password)
+    ((--session-id) . session-id)
+    ((--repl))))
 
-(define (print-result result usage)
-  (if result ; rpc-call returns #f on wrong parameters
-      (let ((reply (vector->list result)))
-        (let ((arguments (cdr (assoc "arguments" reply)))
-              (result (cdr (assoc "result" reply))))
-          (if (string=? result "success")
-              (print arguments)
-              (print result))))
-      (usage (program-name))))
+(define (assoc-val key alist)
+  (let ((key/val (assoc key alist)))
+    (if key/val (cdr key/val) #f)))
+
+(define (init args)
+  (let ((pargs (parse-command-line args *OPTS*)))
+    (let ((username (assoc-val '--username pargs))
+          (password (assoc-val '--password pargs))
+          (port (assoc-val '--port pargs))
+          (session-id (assoc-val '--session-id pargs))
+          (repl? (assoc '--repl pargs)))
+      (when username (*username* username))
+      (when password (*password* password))
+      (when port (*port* port))
+      (when session-id (*session-id* session-id))
+      repl?)))
 
 (define (main args)
-  (let ((username (maybe-list-ref args 0))
-        (password (maybe-list-ref args 1))
-        (rest (maybe-rest-args args)))
-    (parameterize ((*username* username)
-                   (*password* password)
-                   (*session-id* "")) ; 2.3.1 x-transmission-session-id
-      (torrent-get-example rest)
-      ;(torrent-start-example rest)
-      ;(torrent-stop-example rest)
-      )))
+  (if (init args)
+      (repl)
+      (begin
+        (torrent-get-example)
+        (session-get-example))))
 
 (main (command-line-arguments))
